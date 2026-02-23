724f2f3de588f40f2f05fa095f21e1e77675fb8b
Sergey Stoyanov <serghei.stoianov@eservicii.md>
2026-02-23 10:41:08 +0200

    auto: sync dotfiles 2026-02-23 10:41

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,5 @@
 source ~/.config/fish/aliases/aliases.fish
 #source /usr/share/cachyos-fish-config/cachyos-config.fish # Comment for another system
+
+# opencode
+fish_add_path /home/sergey/.opencode/bin
