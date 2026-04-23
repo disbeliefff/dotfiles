31cdd25ba93792e18ef3c5ffeba9d9ecb5b309a9
disbeliefff <sergeycrowler@gmail.com>
2026-04-23 09:59:26 +0300

    auto: sync dotfiles 2026-04-23 09:59

--- fish/functions/gsm-secret-update.fish
+++ fish/functions/gsm-secret-update.fish
@@ -1,4 +1,4 @@
-function su --description "Update GCP Secret Manager secret from a JSON file" --argument-names secret_name file_path
+function sup --description "Update GCP Secret Manager secret from a JSON file" --argument-names secret_name file_path
     if test -z "$secret_name"; or test -z "$file_path"
         echo "Usage: gcp-secret-update <secret-name> <file.json>"
         return 1
