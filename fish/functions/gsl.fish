5d438e8dccd121e19300a5087e5d067998e2af14
disbeliefff <sergeycrowler@gmail.com>
2026-05-30 19:49:15 +0300

    auto: sync dotfiles 2026-05-30 19:49

--- fish/functions/gsl.fish
+++ fish/functions/gsl.fish
@@ -1,8 +1,8 @@
 function gsl --description "List Google Secret Manager secret versions" --argument-names secret_name
-    if test -z "$secret_name"
+    if not set -q secret_name[1]
         echo "Usage: gsl <secret-name>"
         return 1
     end
 
-    gcloud secrets versions list "$secret_name"
+    gcloud secrets versions list $secret_name
 end
