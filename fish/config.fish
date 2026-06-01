da56627d9f31d088b5efa6ab88090e8c819e8c05
disbeliefff <sergeycrowler@gmail.com>
2026-06-01 12:16:21 +0300

    auto: sync dotfiles 2026-06-01 12:16

--- fish/config.fish
+++ fish/config.fish
@@ -65,3 +65,10 @@ function copy
         command cp $argv
     end
 end
+
+# pnpm
+set -gx PNPM_HOME "/home/sergey/.local/share/pnpm"
+if not string match -q -- "$PNPM_HOME/bin" $PATH
+  set -gx PATH "$PNPM_HOME/bin" $PATH
+end
+# pnpm end
